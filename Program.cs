b1fa65a7e456585c0c4326cc7703e9bd61308900
MILAN PANDYA <milan@MILANs-MacBook-Pro.local>
2023-08-16 04:49:11 +1200

    create new record added

--- Program.cs
+++ Program.cs
@@ -20,6 +20,9 @@ passwordTextbox.SendKeys("123123");
 //Identify the login button and click on the button
 IWebElement loginbutton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
 loginbutton.Click();
+
+Thread.Sleep(1000);
+
 //check if user has logged in successfully
 IWebElement helloHarri = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
 if(helloHarri.Text=="Hello hari!")
@@ -30,3 +33,64 @@ else
 {
     Console.WriteLine("User hasn't been logged in");
 }
+
+//Click on Administration dropdown
+IWebElement administrationDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+administrationDropdown.Click();
+
+//Click on Time and Material Option
+IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+tmOption.Click();
+
+
+Thread.Sleep(1000);
+
+//Click on Create new button
+IWebElement createnewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+createnewButton.Click();
+
+//Click on Typecode Dropdown
+IWebElement typecodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
+typecodeDropdown.Click();
+
+//Click on Time Option
+IWebElement timeOption = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
+timeOption.Click();
+
+//Enter value in code
+IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+codeTextbox.SendKeys("Dmd");
+
+//Enter value in Description
+IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+descriptionTextbox.SendKeys("abc1");
+
+
+//Enter value in Price per unit
+IWebElement priceTextbox = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+priceTextbox.SendKeys("367");
+
+//Click on save button
+IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+saveButton.Click();
+
+Thread.Sleep(1000);
+//Check if record added or not
+
+// Check if new Time record has been created successfully
+IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+goToLastPageButton.Click();     
+
+IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+if(newCode.Text == "Dmd")
+
+{
+    Console.WriteLine("Time record has been created successfully.");
+}
+else
+{
+    Console.WriteLine("Time record hasn't been created.");
+}
+
+
+ 
