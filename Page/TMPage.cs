70bf98ccbcfd0a60ae5200289550bdaf7c3b58d6
MILAN PANDYA <milan@MILANs-MacBook-Pro.local>
2023-08-18 02:18:03 +1200

    POM Implimented

--- Page/TMPage.cs
+++ Page/TMPage.cs
@@ -39,10 +39,7 @@ namespace TurnUpPortal.Page
             //Click on save button
             IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
             saveButton.Click();
-            Thread.Sleep(2000);
-
             Thread.Sleep(1000);
-            //Check if record added or not
 
             // Check if new Time record has been created successfully
             IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
@@ -65,25 +62,27 @@ namespace TurnUpPortal.Page
             //Click the Edit button
             IWebElement editButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
             editButton.Click();
+            Thread.Sleep(1000);
 
             //Click on Typecode Dropdown
             IWebElement edittypecodeDropdown1 = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
+            //
             edittypecodeDropdown1.Click();
 
             //Click on Time Option for edit
-            IWebElement edittimeOption = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
+            IWebElement edittimeOption = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[2]"));
             edittimeOption.Click();
 
             //Edit the value in code
             IWebElement editcodeTextbox = driver.FindElement(By.Id("Code"));
             editcodeTextbox.Clear();
-            editcodeTextbox.SendKeys("pott01");
+            editcodeTextbox.SendKeys("August2023");
 
 
             //Edit the value in Description
             IWebElement editdescriptionTextbox = driver.FindElement(By.Id("Description"));
             editdescriptionTextbox.Clear();
-            editdescriptionTextbox.SendKeys("pot01");
+            editdescriptionTextbox.SendKeys("DDD01");
 
             //Edit value in Price per unit
             IWebElement overlappingTag = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
@@ -97,19 +96,47 @@ namespace TurnUpPortal.Page
             IWebElement editsaveButton = driver.FindElement(By.Id("SaveButton"));
             editsaveButton.Click();
             Thread.Sleep(2000);
+
+            //Check if the record has been edited successfully
+            IWebElement gotolastpagebutton1 = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));//*[@id="tmsGrid"]/div[4]/a[4]/span
+            gotolastpagebutton1.Click();
+
+            IWebElement Newdesc = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[3]"));
+            if (Newdesc.Text == "August2023")
+            {
+                Console.WriteLine("New record has been edited successfully");
+
+            }
+            else
+            {
+                Console.WriteLine("New record has not been edited");
+            }
         }
 		public void DeleteTimeRecord(IWebDriver driver)
 		{
+            Thread.Sleep(1000);
             //Click on Delet button
             IWebElement deleteButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[2]"));
             deleteButton.Click();
 
             Thread.Sleep(1000);
             //switch to the alert dialog
-            IAlert alert = driver.SwitchTo().Alert();
+            driver.SwitchTo().Alert().Accept();
 
-            //Accept the alert(Click OK)
-            alert.Accept();
+            IWebElement newcode1 = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+
+            if (newcode1.Text != "August2023")
+            {
+
+                Console.WriteLine(" New record has been successfully deleted");
+
+            }
+            else
+            {
+                Console.WriteLine("New record has not been deleted");
+            }
+
+
         }
 	}
 }
